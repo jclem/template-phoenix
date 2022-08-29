73933c85a28a9cb026a25f10a08212ed91e0e7d7
Jonathan Clem <j@jclem.me>
2022-08-29 14:37:30 -0400

    Set up Phoenix project

---
+++ lib/app.ex
@@ -0,0 +1,30 @@
+defmodule App do
+  @moduledoc """
+  App keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+  """
+  def context do
+    quote do
+      import Ecto.Changeset
+      import Ecto.Query, warn: false
+      import Ecto
+      alias App.Repo
+    end
+  end
+
+  def schema do
+    quote do
+      use Ecto.Schema
+      import Ecto.Changeset
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+end
---
+++ lib/app_web/router.ex
@@ -0,0 +1,61 @@
+defmodule AppWeb.Router do
+  use AppWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, {AppWeb.LayoutView, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  pipeline :admin_only do
+    plug :admin_basic_auth
+  end
+
+  scope "/", AppWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", AppWeb do
+  #   pipe_through :api
+  # end
+
+  scope "/" do
+    import Phoenix.LiveDashboard.Router
+
+    if Mix.env() in [:dev, :test] do
+      pipe_through :browser
+    else
+      pipe_through [:browser, :admins_only]
+    end
+
+    live_dashboard "/dashboard", metrics: AppWeb.Telemetry, ecto_repos: [App.Repo]
+  end
+
+  # Enables the Swoosh mailbox preview in development.
+  #
+  # Note that preview only shows emails that were sent by the same
+  # node running the Phoenix server.
+  if Mix.env() == :dev do
+    scope "/dev" do
+      pipe_through :browser
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  defp admin_basic_auth(conn, _opts) do
+    username = System.fetch_env!("ADMIN_USERNAME")
+    password = System.fetch_env!("ADMIN_PASSWORD")
+    Plug.BasicAuth.basic_auth(conn, username: username, password: password)
+  end
+end
