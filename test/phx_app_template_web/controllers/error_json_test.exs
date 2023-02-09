24a44e350465e15c1456831a262513563ebb2b92
Jonathan Clem <j@jclem.me>
2023-02-09 11:10:10 -0500

    Add ci-deploy workflow

--- test/phx_app_template_web/controllers/error_json_test.exs
+++ test/phx_app_template_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule PhxAppTemplateWeb.ErrorJSONTest do
   use PhxAppTemplateWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert PhxAppTemplateWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert PhxAppTemplateWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
